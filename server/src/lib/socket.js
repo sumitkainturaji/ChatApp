11231aa4f8ab6e9371352de7da364cc7aa1e65ab
Sumit Singh kaintura <98005540+sumitkainturaji@users.noreply.github.com>
2025-06-02 17:56:32 +0530

    Update socket.js

--- server/src/lib/socket.js
+++ server/src/lib/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: ['https://chatapp-6vdf.onrender.com'],
+    origin: ["http://localhost:5173" , "https://chatapp-6vdf.onrender.com"],
         credentials: true,
   },
 })
