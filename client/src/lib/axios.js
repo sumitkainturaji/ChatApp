1d7a3422a036f9309f657d77e887c522b22245b5
Sumit Singh kaintura <98005540+sumitkainturaji@users.noreply.github.com>
2025-06-02 18:08:50 +0530

    Update axios.js

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -2,10 +2,5 @@ import axios from 'axios'
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL + '/api',
-
-  baseURL:
-    import.meta.env.MODE === 'development'
-      ? 'http://localhost:5001/api'
-      : '/api',
   withCredentials: true,
 })
