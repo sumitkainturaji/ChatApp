52a635d7826a55bbbc524b9a11c4e1711bde9c55
sumitkainturaji <kainturasumit190@gmail.com>
2025-08-03 15:30:58 +0530

    sumit updated  code

--- server/src/middelwares/auth.middleware.js
+++ server/src/middelwares/auth.middleware.js
@@ -3,7 +3,7 @@ import User from '../models/user.model.js'
 
 export const protectedRoute = async (req, res, next) => {
   try {
-    const token = req.cookies.jwtToken
+    const token = req.cookies?.jwtToken
     if (!token) {
       res.status(401).json({
         success: false,
